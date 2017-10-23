b914f76a6c9986ef5c7bea57833834eff353c600
Nico Disera <u0869073@utah.edu>
2017-10-23 12:43:52 -0600

    Initial commit

---
+++ app/src/main/java/edu/utah/a4530/cs/hotseatbattleship/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.utah.a4530.cs.hotseatbattleship
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
